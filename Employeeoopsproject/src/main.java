9ba44e90bcdd8a6432bdf5bad377e6b2ec967227
akshitsingh69 <akshitrnq@gmail.com>
2026-04-03 20:45:56 +0530

    Add files via upload

---
+++ Employeeoopsproject/src/main.java
@@ -0,0 +1,106 @@
+package Employeeoopsproject.src;
+
+import java.util.ArrayList;
+
+abstract class Employee{   //abstraction class
+    private String name;
+    private int id;
+    public Employee(String name,int id){ //paramatrise constructor 
+        this.name=name;
+        this.id=id;
+    }
+    public String GetName(){   // getter methord     // we are not giveing direct access of data
+        return name;
+    }
+    public int GetId(){   // getter methord
+        return id;
+    }
+    abstract double CalculateSalary();   //abstract methord 
+    @Override
+    public String toString(){
+        return " Employee[name= "+name+" , Id= "+id+", salary = "+ CalculateSalary()+"]";
+    }
+
+
+}
+class FullTimeEmployee extends Employee{  // showing error if we didnot impliment abstract class methord
+    private double monthlysalary;
+    public FullTimeEmployee (String name, int id , double monthlysalary){ //constructor
+        super(name, id);  // super methord help to run the supperclass oonstructor
+        this.monthlysalary=monthlysalary;
+
+    }
+    @Override
+    public double CalculateSalary(){
+
+        return monthlysalary;
+    }
+
+    
+} 
+class PartTimeEmployee extends Employee{
+    private int hourswork;
+    private double hourlyrate;
+    public PartTimeEmployee(String name, int id, int hourswork,double hourlyrate){
+        super(name, id); // call superclass constructor
+        this.hourswork=hourswork;
+        this.hourlyrate=hourlyrate;
+    }
+    @Override
+    public double CalculateSalary(){
+        return hourswork*hourlyrate;
+    }
+
+}
+class PayRollSystem{
+    private ArrayList<Employee> employeeList;   // initialisation of arraylist   Employee data type
+    public PayRollSystem(){
+        employeeList = new ArrayList<>();  
+
+    }
+    public void AddEmployee(Employee employee){      // employee is the object of Employee
+        employeeList.add(employee);
+    }
+    public void RemoveEmployee(int id){
+        Employee employetoremove = null ;
+        for (Employee employee : employeeList){  //  employee == i   (for each loop)
+            
+            if (employee.GetId()==id){
+                employetoremove = employee;
+                break;
+            }
+            
+        }
+        if(employetoremove!=null){
+            employeeList.remove(employetoremove);
+        }
+    }
+
+        
+        public void DisplayEmployees(){
+        for(Employee employee : employeeList){
+            System.out.println(employee);
+
+        }
+        }
+    }
+
+
+    
+
+public class main {
+    public static void main(String[] args) {
+        PayRollSystem payrollsystem= new PayRollSystem();  // object payrollsystem is created
+        FullTimeEmployee emp1 = new FullTimeEmployee("akshit",1 , 90000);
+        PartTimeEmployee emp2 = new PartTimeEmployee("harsh", 2, 50, 2000);
+        payrollsystem.AddEmployee(emp1);
+        payrollsystem.AddEmployee(emp2);
+        System.out.println("initial employee details: ");
+        payrollsystem.DisplayEmployees();
+        System.out.println(" Remove employee");
+        payrollsystem.RemoveEmployee(2);
+        System.out.println("Remaining employee detail are");
+        payrollsystem.DisplayEmployees();
+    }
+    
+}
