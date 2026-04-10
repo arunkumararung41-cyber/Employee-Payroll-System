fe486a41ac57b04528ecb083b4eb3eaf85f9f5ba
arunkumararung41-cyber <arunkumararung41@gmail.com>
2026-04-10 12:09:47 +0530

    added Employee Payroll System project

--- Employeeoopsproject/src/main.java
+++ Employeeoopsproject/src/main.java
@@ -1,29 +1,28 @@
 package Employeeoopsproject.src;
-
 import java.util.ArrayList;
 
 abstract class Employee{   //abstraction class
     private String name;
     private int id;
-    public Employee(String name,int id){ //paramatrise constructor 
+    public Employee(String name,int id){ //parametrized constructor 
         this.name=name;
         this.id=id;
     }
-    public String GetName(){   // getter methord     // we are not giveing direct access of data
+    public String GetName(){   // getter method     // we are not giving direct access of data
         return name;
     }
-    public int GetId(){   // getter methord
+    public int GetId(){   // getter method
         return id;
     }
-    abstract double CalculateSalary();   //abstract methord 
+    abstract double CalculateSalary();   //abstract method 
     @Override
     public String toString(){
-        return " Employee[name= "+name+" , Id= "+id+", salary = "+ CalculateSalary()+"]";
+        return " Employee:-name: "+name+" | Id: "+id+" | salary: "+ CalculateSalary()+"";
     }
 
 
 }
-class FullTimeEmployee extends Employee{  // showing error if we didnot impliment abstract class methord
+class FullTimeEmployee extends Employee{  // showing error if we did not implement abstract class method
     private double monthlysalary;
     public FullTimeEmployee (String name, int id , double monthlysalary){ //constructor
         super(name, id);  // super methord help to run the supperclass oonstructor
@@ -53,7 +52,7 @@ class PartTimeEmployee extends Employee{
 
 }
 class PayRollSystem{
-    private ArrayList<Employee> employeeList;   // initialisation of arraylist   Employee data type
+    private ArrayList<Employee> employeeList;   // initialization of arraylist   Employee data type
     public PayRollSystem(){
         employeeList = new ArrayList<>();  
 
@@ -88,18 +87,18 @@ class PayRollSystem{
 
     
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         PayRollSystem payrollsystem= new PayRollSystem();  // object payrollsystem is created
-        FullTimeEmployee emp1 = new FullTimeEmployee("akshit",1 , 90000);
-        PartTimeEmployee emp2 = new PartTimeEmployee("harsh", 2, 50, 2000);
-        payrollsystem.AddEmployee(emp1);
-        payrollsystem.AddEmployee(emp2);
+        FullTimeEmployee fullTimeEmployee = new FullTimeEmployee("arun",1 , 90000);
+        PartTimeEmployee partTimeEmployee = new PartTimeEmployee("sanjay", 2, 50, 2000);
+        payrollsystem.AddEmployee(fullTimeEmployee);
+        payrollsystem.AddEmployee(partTimeEmployee);
         System.out.println("initial employee details: ");
         payrollsystem.DisplayEmployees();
-        System.out.println(" Remove employee");
+        System.out.println(" Remove employee id 2");
         payrollsystem.RemoveEmployee(2);
-        System.out.println("Remaining employee detail are");
+        System.out.println("Remaining employee details are");
         payrollsystem.DisplayEmployees();
     }
     
